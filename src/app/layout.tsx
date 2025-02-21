f54197fb3a96a8e8db060c1007ba69e107c61b74
estvc72024 <esteban.vasquez.cardenas@intel.com>
2025-02-21 16:41:47 -0600

    feat: add speed insignths

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { siteDetails } from '@/data/siteDetails';
 import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 import "./globals.css";
 
@@ -55,6 +56,7 @@ export default function RootLayout({
         </main>
         <Footer />
         <Analytics/>
+        <SpeedInsights />
       </body>
     </html>
   );
